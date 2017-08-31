cfae5502f43ca89fc6c5edaa1ef9ed46b55733fb
Matthew C Kunzer <matthewkunzer@gmail.com>
2017-08-31 11:38:12 -0500

    7777

--- MapDemo.java
+++ MapDemo.java
@@ -10,17 +10,30 @@ public class MapDemo
 {
    public static void main(String[] args)
    {
-	   //create Map: favoriteColors
-
-
-	  //create Set<String> keySet
-
+       //create Map: favoriteColors
+       Map<String , Color> favoriteColors = new HashMap<>();
+       favoriteColors.put("Juliet", Color.BLUE);
+       favoriteColors.put("Romeo", Color.GREEN);
+       favoriteColors.put("Adam", Color.RED);
+       favoriteColors.put("Eve", Color.BLUE);
+      //create Set<String> keySet
+      Set<String> keySet = favoriteColors.keySet();
       // Print all keys and values in the map
+      
 
-
-      for ()
+      for (String key: keySet)
       {
-
+          if (key.equals("Romeo")){
+              favoriteColors.remove(key);
+            }
+          
+          Color value = favoriteColors.get(key);
+          if(value == Color.BLUE){
+              favoriteColors.remove(key);
+            }
+          System.out.println(key + " : " + value);
       }
+      
+      
    }
 }
