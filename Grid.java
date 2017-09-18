7b073f7fecee00034907a28797ab4ae73d6d1b66
Matthew C Kunzer <matthewkunzer@gmail.com>
2017-09-18 12:22:23 -0500

    yuhj

--- Grid.java
+++ Grid.java
@@ -2,14 +2,26 @@ public class Grid
 {
    private static final int SIZE = 10;
    int[][] pixels = new int[SIZE][SIZE];
-   . . .
-
+   static int number = 0;
    /**
       Flood fill, starting with the given row and column.
    */
    public void floodfill(int row, int column)
    {
-      . . .
+      this.pixels[row][column] = number;
+      number++;
+      if(column + 1 < 10 && pixels[row][column+1] == 0){
+          floodfill(row, column +1 );
+        }
+      if(row + 1 < 10 && pixels[row+1][column] == 0){
+          floodfill(row+1,column);
+        }
+      if(column - 1 > -1 && pixels[row][column-1] == 0){
+          floodfill(row,column-1);
+        }
+      if(row -1 > -1 && pixels[row-1][column] == 0) {
+          floodfill(row-1,column);
+        }
    }
 
    @Override
